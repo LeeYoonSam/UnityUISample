e5c8cbbe08a3251d61ad9a39496e3e7c45867aab
YS <yoonsam86@gmail.com>
2017-08-15 13:03:40 +0900

    UI Sample Start

---
+++ Assets/_Script/SplashScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashScript : MonoBehaviour {
+
+	public float delayTime = 3;
+
+
+	IEnumerator Start () {
+		yield return new WaitForSeconds (delayTime);
+		Application.LoadLevel ("LobbyScene");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
